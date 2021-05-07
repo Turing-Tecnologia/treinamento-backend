3ab09132d0d3493817b4e648d74caf49c1471bf3
Cledson Ferreira <cledsonitgames@gmail.com>
2021-05-07 04:04:06 -0300

    Documentação

--- src/main/java/com/turingtecnologia/albatroz/backendalbatroz/controller/ClinicaController.java
+++ src/main/java/com/turingtecnologia/albatroz/backendalbatroz/controller/ClinicaController.java
@@ -1,5 +1,7 @@
 package com.turingtecnologia.albatroz.backendalbatroz.controller;
 
+import javax.validation.Valid;
+
 import com.turingtecnologia.albatroz.backendalbatroz.dto.ClinicaDTO;
 import com.turingtecnologia.albatroz.backendalbatroz.model.entities.Clinica;
 import com.turingtecnologia.albatroz.backendalbatroz.services.interfaces.ClinicaService;
@@ -16,7 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
+import lombok.val;
 
 @RestController
 @RequestMapping(path = "/clinicas")
@@ -24,29 +30,54 @@ import lombok.AllArgsConstructor;
 public class ClinicaController {
     final ClinicaService service;
 
+    @ApiOperation(value = "Salva a clínica no Banco de Dados.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Clínica salva com sucesso."),
+        @ApiResponse(code = 400, message = "Parâmetros incorretos na requisição."),
+        @ApiResponse(code = 500, message = "Erro ao salvar a clínica.")
+    })
     @PostMapping
-    public ResponseEntity<Clinica> save(@RequestBody ClinicaDTO dto) {
+    public ResponseEntity<Clinica> save(@RequestBody @Valid ClinicaDTO dto) {
         Clinica clinica = new ModelMapper().map(dto, Clinica.class);
         clinica = service.addClinica(clinica);
         return new ResponseEntity<>(clinica, HttpStatus.CREATED);
     }
 
+    @ApiOperation("Procura uma clínica pelo CNPJ.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Clínica encontrada."),
+        @ApiResponse(code = 404, message = "CNPJ informado não existe."),
+        @ApiResponse(code = 500, message = "Erro ao procurar clínica.")
+    })
     @GetMapping("/{cnpjClinica}")
     public ResponseEntity<Clinica> get(@PathVariable String cnpjClinica) {
         Clinica clinica = service.findByCnpjClinica(cnpjClinica);
         return new ResponseEntity<>(clinica, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Atualiza os dados de uma clínica.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Cínica atualizada com sucesso."),
+        @ApiResponse(code = 400, message = "Parâmetros incorretos na requisição."),
+        @ApiResponse(code = 404, message = "CNPJ informado não existe."),
+        @ApiResponse(code = 500, message = "Erro ao salvar a clínica.")
+    })
     @PutMapping("/{cnpjClinica}")
     public ResponseEntity<Clinica> update(
         @PathVariable String cnpjClinica,
-        @RequestBody ClinicaDTO dto
+        @RequestBody @Valid ClinicaDTO dto
     ) {
         Clinica clinica = new ModelMapper().map(dto, Clinica.class);
         clinica = service.updateClinica(cnpjClinica, clinica);
         return new ResponseEntity<>(clinica, HttpStatus.OK);
     }
 
+    @ApiOperation("Deleta uma clínica pelo CNPJ.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Clínica deletada com sucesso."),
+        @ApiResponse(code = 404, message = "CNPJ informado não existe."),
+        @ApiResponse(code = 500, message = "Erro ao procurar clínica.")
+    })
     @DeleteMapping("/{cnpjClinica}")
     public ResponseEntity<Void> delete(@PathVariable String cnpjClinica) {
         service.deleteClinica(cnpjClinica);
