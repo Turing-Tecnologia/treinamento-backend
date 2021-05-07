65fb838cbc67d6409dcaca6dd1f17710cf5f7001
Malyson Breno <7494537+malysonb@users.noreply.github.com>
2021-05-07 13:48:29 -0300

    Documentando pelo swagger

--- src/main/java/com/turingtecnologia/albatroz/backendalbatroz/controller/ClinicaController.java
+++ src/main/java/com/turingtecnologia/albatroz/backendalbatroz/controller/ClinicaController.java
@@ -19,6 +19,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/admin/clinicas")
 public class ClinicaController {
@@ -26,21 +30,50 @@ public class ClinicaController {
     @Autowired
     private ClinicaService service;
 
+    @ApiOperation(value = "Realizar uma busca de clinica, pelo cnpj da clinica.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Busca realizada com sucesso."),
+            @ApiResponse(code = 400, message = "Falta o dado(cnpj) para se realizar a busca."),
+            @ApiResponse(code = 500, message = "Erro ao processar a busca.")
+    })
     @GetMapping("/{cnpj}")
     public ResponseEntity<Clinica> getClinica(@PathVariable("cnpj") @CNPJ String cnpj){
         return new ResponseEntity<>(service.getClinica(cnpj), HttpStatus.CREATED);
     }
 
+    /*----------------*/
+
+    @ApiOperation(value = "Cadastro de clinica.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Clinica cadastrada com sucesso."),
+            @ApiResponse(code = 400, message = "Falta dados para se realizar o cadastro."),
+            @ApiResponse(code = 500, message = "Erro ao processar o cadastro.")
+    })
     @PostMapping
     public ResponseEntity<Clinica> adicionarClinica(@RequestBody @Valid ClinicaDTO dto){
         return new ResponseEntity<>(service.salvar(dto),HttpStatus.CREATED);
     }
 
+    /*----------------*/
+    @ApiOperation(value = "Edita a clinica do banco de dados, pelo cnpj e objeto da clinica.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Clinica edita com sucesso."),
+            @ApiResponse(code = 400, message = "Falta o dados para editar a clinica."),
+            @ApiResponse(code = 500, message = "Erro ao processar a edição!.")
+    })
     @PutMapping("/{cnpj}")
     public ResponseEntity<Clinica> editarClinica(@PathVariable("cnpj") @CNPJ String cnpj, @RequestBody @Valid ClinicaDTO dto){
         return new ResponseEntity<>(service.editar(cnpj,dto), HttpStatus.CREATED);
     }
 
+    /*----------------*/
+
+    @ApiOperation(value = "Deleta a clinica do banco de dados, pelo cnpj da clinica.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Clinica deletada com sucesso."),
+            @ApiResponse(code = 400, message = "Falta o dado(cnpj) para deletar a clinica."),
+            @ApiResponse(code = 500, message = "Erro ao processar o delete.")
+    })
     @DeleteMapping("/{cnpj}")
     public ResponseEntity<Void> removerClinica(@PathVariable("cnpj") @CNPJ String cnpj){
         service.excluir(cnpj);
--- src/main/java/com/turingtecnologia/albatroz/backendalbatroz/dto/ClinicaDTO.java
+++ src/main/java/com/turingtecnologia/albatroz/backendalbatroz/dto/ClinicaDTO.java
@@ -8,11 +8,11 @@ import lombok.Data;
 
 @Data
 public class ClinicaDTO {
-    @NotBlank
+    @NotBlank(message = "nomeClinica {not.blank}")
     private String nomeClinica;
-    @NotBlank
+    @NotBlank(message = "telefone {not.blank}")
     private String telefone;
-    @NotBlank
+    @NotBlank(message = "email {not.blank}")
     private String email;
     @CNPJ
     private String cnpj;
