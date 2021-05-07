b285075376311416a17032f99b56fe1ffd0d11ae
Malyson Breno <7494537+malysonb@users.noreply.github.com>
2021-05-07 05:14:09 -0300

    Removendo temporariamente validacao

--- src/main/java/com/turingtecnologia/albatroz/backendalbatroz/dto/ClinicaDTO.java
+++ src/main/java/com/turingtecnologia/albatroz/backendalbatroz/dto/ClinicaDTO.java
@@ -2,8 +2,6 @@ package com.turingtecnologia.albatroz.backendalbatroz.dto;
 
 import javax.validation.constraints.NotBlank;
 
-import org.hibernate.validator.constraints.br.CNPJ;
-
 import lombok.Data;
 
 @Data
@@ -14,6 +12,6 @@ public class ClinicaDTO {
     private String telefone;
     @NotBlank
     private String email;
-    @CNPJ
+    @NotBlank
     private String cnpj;
 }
