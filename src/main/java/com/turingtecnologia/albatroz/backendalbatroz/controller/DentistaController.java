21dba60870a625b8a9c0af167f72197f7af99647
luancad <lludd.c.a.d@hotmail.com>
2021-05-11 20:38:41 -0300

    Inserindo a documentação no swagger

--- src/main/java/com/turingtecnologia/albatroz/backendalbatroz/controller/DentistaController.java
+++ src/main/java/com/turingtecnologia/albatroz/backendalbatroz/controller/DentistaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,7 +54,7 @@ public class DentistaController {
             @ApiResponse(code = 400, message = "Falta o dado(id) para se realizar a busca."),
             @ApiResponse(code = 500, message = "Erro ao processar a busca.")
     })
-    @GetMapping(value = "/{idDentista}")
+    @GetMapping(value = "id/{idDentista}")
     public ResponseEntity<Dentista> busca(@PathVariable("idDentista") Long id) {
         return new ResponseEntity<>(dentistaService.findByIdDentista(id), HttpStatus.OK);
     }
@@ -66,6 +67,42 @@ public class DentistaController {
     @GetMapping(value = "/dentistas")
     public ResponseEntity<List<Dentista>> listaDentistas() {
         return new ResponseEntity<>(dentistaService.findAllDentistas(), HttpStatus.OK);
+    }
+    
+    @ApiOperation(value = "Apaga o dentista do banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Dentista apagado com sucesso"),
+            @ApiResponse(code = 500, message = "Erro ao deletar o dentista.")
+    })
+    
+    @DeleteMapping(value = "remove/dentistas")
+    public ResponseEntity<Void> removeDentista(@Valid @RequestBody DentistaDTO dentistaDTO) {
+    	Dentista dentista = new ModelMapper().map(dentistaDTO, Dentista.class);
+        dentistaService.removeDentista(dentista);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    
+    @ApiOperation(value = "Busca um dentista pelo CPF.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Busca realizada com sucesso."),
+            @ApiResponse(code = 400, message = "Faltam dados para realizar a busca."),
+            @ApiResponse(code = 500, message = "Erro ao processar a busca.")
+    })
+    @GetMapping(value = "cpf/{cpfDentista}")
+    public ResponseEntity<Dentista> search(@PathVariable(value = "cpfDentista") String cpfDentista) {
+        return new ResponseEntity<>(dentistaService.findByCpfDentista(cpfDentista), HttpStatus.OK);
+    }
+    
+    @ApiOperation(value = "Atualiza os dados do cliente.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Dados atualizados com sucesso."),
+            @ApiResponse(code = 400, message = "Estar faltando dados para realizar a atualização."),
+            @ApiResponse(code = 500, message = "Erro ao atualizar o cliente.")
+    })
+    @PutMapping(value = "altera/dentistas")
+    public ResponseEntity<Dentista> alteraDentista(@Valid @RequestBody DentistaDTO dentistaDTO) {
+    	Dentista dentista = new ModelMapper().map(dentistaDTO, Dentista.class);
+        return new ResponseEntity<>(dentistaService.alteraDentista(dentista), HttpStatus.CREATED);
     }
     
 }
